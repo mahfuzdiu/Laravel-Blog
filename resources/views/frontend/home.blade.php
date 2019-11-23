844b5e6fdf6c2066d9e92fd4955f1ea78423306c
mahfuzdiu <mahfuz.dev@gmail.com>
2019-11-23 22:14:29 +0600

    reduced extra content

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -34,151 +34,7 @@
                                 </div>
                             </div>
                         </div>
-                        <div class="col-md-6">
-                            <div class="blog-entry ftco-animate">
-                                <a href="#" class="img img-2" style="background-image: url(images/image_2.jpg);"></a>
-                                <div class="text text-2 pt-3 mt-3">
-                                    <span class="big">Asia</span>
-                                    <h3 class="mb-4"><a href="#">What to pack when visiting Philippines in summer time</a></h3>
-                                    <p class="mb-4">Even the all-powerful Pointing has no control about the blind texts it is an almost</p>
-                                    <div class="author mb-4 d-flex align-items-center">
-                                        <a href="#" class="img" style="background-image: url(images/person_2.jpg);"></a>
-                                        <div class="ml-3 info">
-                                            <span>Written by</span>
-                                            <h3><a href="#">Dave Lewis</a>, <span>October 04, 2018</span></h3>
-                                        </div>
-                                    </div>
-                                    <div class="meta-wrap align-items-center">
-                                        <div class="half order-md-last">
-                                            <p class="meta">
-                                                <span><i class="icon-heart"></i>3</span>
-                                                <span><i class="icon-eye"></i>100</span>
-                                                <span><i class="icon-comment"></i>5</span>
-                                            </p>
-                                        </div>
-                                        <div class="half">
-                                            <p><a href="#" class="btn btn-primary px-3 py-2">Continue Reading</a></p>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-md-6">
-                            <div class="blog-entry ftco-animate">
-                                <a href="#" class="img img-2" style="background-image: url(images/image_3.jpg);"></a>
-                                <div class="text text-2 pt-3 mt-3">
-                                    <span class="big">Asia</span>
-                                    <h3 class="mb-4"><a href="#">What to pack when visiting Philippines in summer time</a></h3>
-                                    <p class="mb-4">Even the all-powerful Pointing has no control about the blind texts it is an almost</p>
-                                    <div class="author mb-4 d-flex align-items-center">
-                                        <a href="#" class="img" style="background-image: url(images/person_2.jpg);"></a>
-                                        <div class="ml-3 info">
-                                            <span>Written by</span>
-                                            <h3><a href="#">Dave Lewis</a>, <span>October 04, 2018</span></h3>
-                                        </div>
-                                    </div>
-                                    <div class="meta-wrap align-items-center">
-                                        <div class="half order-md-last">
-                                            <p class="meta">
-                                                <span><i class="icon-heart"></i>3</span>
-                                                <span><i class="icon-eye"></i>100</span>
-                                                <span><i class="icon-comment"></i>5</span>
-                                            </p>
-                                        </div>
-                                        <div class="half">
-                                            <p><a href="#" class="btn btn-primary px-3 py-2">Continue Reading</a></p>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-md-6">
-                            <div class="blog-entry ftco-animate">
-                                <a href="#" class="img img-2" style="background-image: url(images/image_4.jpg);"></a>
-                                <div class="text text-2 pt-3 mt-3">
-                                    <span class="big">Asia</span>
-                                    <h3 class="mb-4"><a href="#">What to pack when visiting Philippines in summer time</a></h3>
-                                    <p class="mb-4">Even the all-powerful Pointing has no control about the blind texts it is an almost</p>
-                                    <div class="author mb-4 d-flex align-items-center">
-                                        <a href="#" class="img" style="background-image: url(images/person_2.jpg);"></a>
-                                        <div class="ml-3 info">
-                                            <span>Written by</span>
-                                            <h3><a href="#">Dave Lewis</a>, <span>October 04, 2018</span></h3>
-                                        </div>
-                                    </div>
-                                    <div class="meta-wrap align-items-center">
-                                        <div class="half order-md-last">
-                                            <p class="meta">
-                                                <span><i class="icon-heart"></i>3</span>
-                                                <span><i class="icon-eye"></i>100</span>
-                                                <span><i class="icon-comment"></i>5</span>
-                                            </p>
-                                        </div>
-                                        <div class="half">
-                                            <p><a href="#" class="btn btn-primary px-3 py-2">Continue Reading</a></p>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-md-6">
-                            <div class="blog-entry ftco-animate">
-                                <a href="#" class="img img-2" style="background-image: url(images/image_5.jpg);"></a>
-                                <div class="text text-2 pt-3 mt-3">
-                                    <span class="big">Asia</span>
-                                    <h3 class="mb-4"><a href="#">What to pack when visiting Philippines in summer time</a></h3>
-                                    <p class="mb-4">Even the all-powerful Pointing has no control about the blind texts it is an almost</p>
-                                    <div class="author mb-4 d-flex align-items-center">
-                                        <a href="#" class="img" style="background-image: url(images/person_2.jpg);"></a>
-                                        <div class="ml-3 info">
-                                            <span>Written by</span>
-                                            <h3><a href="#">Dave Lewis</a>, <span>October 04, 2018</span></h3>
-                                        </div>
-                                    </div>
-                                    <div class="meta-wrap align-items-center">
-                                        <div class="half order-md-last">
-                                            <p class="meta">
-                                                <span><i class="icon-heart"></i>3</span>
-                                                <span><i class="icon-eye"></i>100</span>
-                                                <span><i class="icon-comment"></i>5</span>
-                                            </p>
-                                        </div>
-                                        <div class="half">
-                                            <p><a href="#" class="btn btn-primary px-3 py-2">Continue Reading</a></p>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-md-6">
-                            <div class="blog-entry ftco-animate">
-                                <a href="#" class="img img-2" style="background-image: url(images/image_6.jpg);"></a>
-                                <div class="text text-2 pt-3 mt-3">
-                                    <span class="big">Asia</span>
-                                    <h3 class="mb-4"><a href="#">What to pack when visiting Philippines in summer time</a></h3>
-                                    <p class="mb-4">Even the all-powerful Pointing has no control about the blind texts it is an almost</p>
-                                    <div class="author mb-4 d-flex align-items-center">
-                                        <a href="#" class="img" style="background-image: url(images/person_2.jpg);"></a>
-                                        <div class="ml-3 info">
-                                            <span>Written by</span>
-                                            <h3><a href="#">Dave Lewis</a>, <span>October 04, 2018</span></h3>
-                                        </div>
-                                    </div>
-                                    <div class="meta-wrap align-items-center">
-                                        <div class="half order-md-last">
-                                            <p class="meta">
-                                                <span><i class="icon-heart"></i>3</span>
-                                                <span><i class="icon-eye"></i>100</span>
-                                                <span><i class="icon-comment"></i>5</span>
-                                            </p>
-                                        </div>
-                                        <div class="half">
-                                            <p><a href="#" class="btn btn-primary px-3 py-2">Continue Reading</a></p>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+
                     </div>
                     <div class="row mt-5">
                         <div class="col text-center">
@@ -187,9 +43,6 @@
                                     <li><a href="#">&lt;</a></li>
                                     <li class="active"><span>1</span></li>
                                     <li><a href="#">2</a></li>
-                                    <li><a href="#">3</a></li>
-                                    <li><a href="#">4</a></li>
-                                    <li><a href="#">5</a></li>
                                     <li><a href="#">&gt;</a></li>
                                 </ul>
                             </div>
@@ -209,11 +62,6 @@
                         <h3>Destination</h3>
                         <ul class="categories">
                             <li><a href="#">Africa <span>(6)</span></a></li>
-                            <li><a href="#">Asia <span>(8)</span></a></li>
-                            <li><a href="#">Australia <span>(2)</span></a></li>
-                            <li><a href="#">Europe <span>(2)</span></a></li>
-                            <li><a href="#">North America <span>(7)</span></a></li>
-                            <li><a href="#">South America <span>(5)</span></a></li>
                         </ul>
                     </div>
 
@@ -221,28 +69,6 @@
                         <h3>Popular Articles</h3>
                         <div class="block-21 mb-4 d-flex">
                             <a class="blog-img mr-4" style="background-image: url(images/image_1.jpg);"></a>
-                            <div class="text">
-                                <h3 class="heading"><a href="#">Even the all-powerful Pointing has no control about the blind texts</a></h3>
-                                <div class="meta">
-                                    <div><a href="#"><span class="icon-calendar"></span> Oct. 04, 2018</a></div>
-                                    <div><a href="#"><span class="icon-person"></span> Dave Lewis</a></div>
-                                    <div><a href="#"><span class="icon-chat"></span> 19</a></div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="block-21 mb-4 d-flex">
-                            <a class="blog-img mr-4" style="background-image: url(images/image_2.jpg);"></a>
-                            <div class="text">
-                                <h3 class="heading"><a href="#">Even the all-powerful Pointing has no control about the blind texts</a></h3>
-                                <div class="meta">
-                                    <div><a href="#"><span class="icon-calendar"></span> Oct. 04, 2018</a></div>
-                                    <div><a href="#"><span class="icon-person"></span> Dave Lewis</a></div>
-                                    <div><a href="#"><span class="icon-chat"></span> 19</a></div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="block-21 mb-4 d-flex">
-                            <a class="blog-img mr-4" style="background-image: url(images/image_3.jpg);"></a>
                             <div class="text">
                                 <h3 class="heading"><a href="#">Even the all-powerful Pointing has no control about the blind texts</a></h3>
                                 <div class="meta">
